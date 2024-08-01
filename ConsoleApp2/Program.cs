ac99a1dc33d77a7c30058aeb8925cb8185889e75
mohiitt <mohitbarade1@gmail.com>
2024-08-01 09:28:41 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+internal class Program
+{
+static void Main(string[] args)
+{
+string characterName = "john";
+int characterAge;
+characterAge = 35;
+//Console.WriteLine("name of character is " + characterName);
+            
+int age = 30;
+double gpa = 3.3;
+char grade='A';
+bool ismale = false;
+
+//stringmanipulation
+string phrase = "giraffe academy";
+//Console.WriteLine(phrase.Length);
+//Console.WriteLine(phrase.ToLower());
+//Console.WriteLine(phrase.Contains("academy"));
+            //Console.WriteLine(phrase[2]);
+            //Console.WriteLine(phrase.IndexOf('i'));
+
+            
+            //Console.ReadLine();
+
+            //user input
+            //Console.Write("Enter your name : ");
+            string name = Console.ReadLine();
+           // Console.WriteLine(name);
+
+            // calculator
+
+
+        }
+}
+}
